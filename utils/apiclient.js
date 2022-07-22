ef308f7f7c2006870df957d1d45d8e8b6f30a2b7
Jordan Walsh <jordan.walsh@postman.com>
2022-07-22 17:03:16 +1000

    init

--- utils/apiclient.js
+++ utils/apiclient.js
@@ -3,7 +3,7 @@ const axios = require('axios').default
 
 //Postman API Key
 const APIKEY =
-  'PMAK-62da1fedd857466b3f382ad6-8f741661f1c57474f17a18587b381267d9'
+  ''
 const POSTMAN_API_URL = 'https://api.getpostman.com'
 
 const instance = axios.create({
